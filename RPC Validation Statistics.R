e01733da21f67f409592555ff8ab6945d5fb69a6
Tim McClure <66977121+tmcclure74@users.noreply.github.com>
2022-06-14 20:33:11 -0700

    Originally committed a version old of the script, not realizing there was a "do you want to overwrite" in the background as I set up the new rProj with the github clone. This is a slightly updated version with some clarifying comments and things added back that I'd meant to originally include so that the process that led to this code was more clear.

--- RPC Validation Statistics.R
+++ RPC Validation Statistics.R
@@ -9,11 +9,11 @@ library(boot)
 #Grab data and convert Species Code to a Factor
 #-----------------------------
 
-quad_rpc_data <- read_csv("Quadrat_RPC_2022_Data.csv") %>%
-  mutate(Species = as.character(Species))
-rpc_data <- read_csv("RPC_2022_RawData_Export.csv") %>%
-  mutate(Species = as.character(Species))
+quad_rpc_data <- read_csv("Quadrat_RPC_2022_Data.csv") 
+rpc_data <- read_csv("RPC_2022_RawData_Export.csv") 
+#leaving species as numeric because it makes the tables order more sensibly for me being familiar with the RPC species and their codes
 
+#---------------------------------------------------------------------------
 #-----------------------------
 #Calculate average proportions for each meter point ("old" RPCs) or each quadrat point, then
 #Calculate average proportions across entire site, then
@@ -43,6 +43,11 @@ Pipe_ChiSq_By_Species <- Combined_Data %>%
   summarize(ChiSq_p_val = case_when(average_proportion == 0 && quad_average_proportion == 0 ~ 1,
                                     average_proportion != 0 && quad_average_proportion != 0 ~ chisq.test(average_proportion, quad_average_proportion)$p.value)) #turns out this doesn't need to be .$average_proportion , etc
 
+Testing_Summarized_ChiSq <- Combined_Data %>%
+  filter(Species == 2013) %>%
+  summarize(ChiSq_p_val = case_when(all(average_proportion) == 0 && all(quad_average_proportion) == 0 ~ 1,
+                                    all(average_proportion) != 0 && all(quad_average_proportion) != 0 ~ chisq.test(average_proportion, quad_average_proportion)$p.value)) #turns out this doesn't need to be .$average_proportion , etc
+
 #What's going on here, we're getting the same p-value for every single species, something isn't right in summarize
 #as demonstrated by below - we get different p-values for individual species as expected.
 
@@ -50,15 +55,17 @@ Testing_SummarizedChiSquared <- Combined_Data %>%
   filter(Species == "11010") %>%
   summarize(ChiSq_p_val = chisq.test(average_proportion, quad_average_proportion)$p.value)
 
+#AHH except for the cases with all zeroes
 Testing_SummarizedChiSquared2 <- Combined_Data %>%
   filter(Species == "2013")
-  summarize(Testing_SummarizedChiSquared2, ChiSq_p_val = chisq.test(average_proportion, quad_average_proportion)$p.value)
+summarize(Testing_SummarizedChiSquared2, ChiSq_p_val = chisq.test(average_proportion, quad_average_proportion)$p.value)
 
+#-----------------------------
 # So much for trying to be a proper-functional programming R user, back to object oriented/loops since clearly the filter works on each individual spp.
+#-----------------------------
 
 ChiSq_By_Species <- data.frame(Species=character(), ScientificName=character(), CommonName=character(), P_Value=double())
 Species_List <- as.character(unique(Combined_Data$Species))
-
 
 for(i in 1:Species_List){
   j <- Species_List[i]
@@ -82,7 +89,7 @@ for(i in 1:Species_List){
   }
 }
 
-#-----------------------------
+#---------------------------------------------------------------------------
 
 #-----------------------------
 #Not the cause of the issues above, but for a potentially more robust analysis - 
@@ -117,9 +124,13 @@ Boot_ChiSq_By_Species <- Boot_Combined_Data %>%
   group_by(Species, ScientificName, CommonName) %>%
   summarize(ChiSq_p_val = chisq.test(bootstrapped_mean, quad_bootstrapped_mean)$p.value)
 
-Boot_Test <- Boot_Combined_Data %>%
-  filter(Species == "11010") %>%
-  summarize(ChiSq_p_val = chisq.test(bootstrapped_mean, quad_bootstrapped_mean)$p.value)
-
 # (Same issues as above, something's up with summarize, but built this thinking something was happening because of a low n(), 
 #  but this is potentially more robust anyways so I'm not throwing away this code yet)
+
+#  LATER NOTE - so my original issue with the summarize was I didn't need to do
+# summarize(ChiSq_p_val = chisq.test(.$bootstrapped_mean, .$quad_bootstrapped_mean)$p.value)
+# and that .$ wasn't necessary even switching out of tidyverse functions - so that was a cool note to learn, but that weirdly fixed
+# my issues with this one, but not the above one - 
+
+# LATER NOTE #2 my underlying issue, now that I'm not getting all the same p-values across the summarize 
+# (IE .$ seems to eliminate the group_by() ), is that I figured out why chisq.test was misbehaving (obviously) on all zeroes.
